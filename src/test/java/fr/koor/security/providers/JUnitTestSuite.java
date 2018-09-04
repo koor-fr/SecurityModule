d95e490ec29b9fd00027966790d35b4e4d56209b
Dominique Liard <dominique.liard@infini-software.com>
2018-09-04 20:26:36 +0200

    UnitTests

---
+++ src/test/java/fr/koor/security/providers/JUnitTestSuite.java
@@ -0,0 +1,14 @@
+package fr.koor.security.providers;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)				
+@Suite.SuiteClasses({				
+	JdbcSecurityManagerCoreTest_Derby.class,
+	JdbcSecurityManagerCoreTest_MySql.class,
+	XmlSecurityManagerCoreTest.class,
+	XmlSecurityManagerCoreTest2.class
+})		
+public class JUnitTestSuite {				
+}
